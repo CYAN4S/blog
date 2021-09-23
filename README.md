5bf99535e52eb03fb7eb5258c264e2795aff47e9
CYAN4S <me@cyan4s.com>
2021-09-23 23:59:15 +0900

    redesign

---
+++ README.md
@@ -0,0 +1,13 @@
+# CYAN4S BLOG
+
+## Development Environment
+
+### Jekyll
+
+```bash
+bundle exec jekyll serve --drafts
+```
+
+```bash
+bundle exec jekyll serve --drafts --host 0.0.0.0
+```
--- assets/js/default.js
+++ assets/js/default.js
@@ -2,3 +2,9 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function turn() {
+  document.body.classList.contains("dark")
+    ? document.body.classList.remove("dark")
+    : document.body.classList.add("dark");
+}
